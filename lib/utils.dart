dc7a8b44f8c508f18bd89dc2431c9a4714b3469e
iprinceisah <princeisah15@gmail.com>
2020-08-17 13:52:44 -0400

    first commit

---
+++ lib/utils.dart
@@ -0,0 +1,16 @@
+import 'dart:html';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const BightGery = Color(0xff649D66);
+  static const BlackColor = Color(0xff000000);
+  static const BackGroundColor = Color(0xffE5E5E5);
+  static const TextFiledBackroundColor = Color(0xffE9F2EA);
+  static const BoldTextColor = Color(0xff292E2C);
+  static const DeepGeryColor = Color(0xff06623B);
+  static const NotSelectedColor = Color(0xff34413B);
+  static const MassageBoxColor = Color(0X649D66);
+  static const YellowColors = Color(0xffF6F578);
+}
